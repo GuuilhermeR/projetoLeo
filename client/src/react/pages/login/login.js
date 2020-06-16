a790b80dd406acc81a620bec65d77f3784033af7
GuuilhermeR <42014565+GuuilhermeR@users.noreply.github.com>
2020-06-16 20:29:42 -0300

    Tela de Login

---
+++ client/src/react/pages/login/login.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import Header from '../../header/header';
+import CardHome from '../../body/home/cardHome';
+import Footer from '../../footer/footer';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header title="MG Revendedora"/>
+                <CardHome />
+                <Footer />
+            </Fragment>
+        )
+    }
+}
+export default Home;
