da1ae8c7041efd715b265e4c7ebb7cf335faae4f
mhenr <matheushhorongoso@gmail.com>
2020-06-16 21:47:25 -0300

    criado icones de login

--- client/src/react/body/login/login.js
+++ client/src/react/body/login/login.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import LockTwoToneIcon from '@material-ui/icons/LockTwoTone';
+import VisibilityTwoToneIcon from '@material-ui/icons/VisibilityTwoTone';
+import VisibilityOffTwoToneIcon from '@material-ui/icons/VisibilityOffTwoTone';
+import AccountCircleTwoToneIcon from '@material-ui/icons/AccountCircleTwoTone';
 import './login.css';
+
+const view_password = () =>{
+  let passwordField = document.getElementById("PasswordField");
+  let visibleIcon = document.getElementById("visible-icon");
+  let visibleIconOff = document.getElementById("visible-icon-off");
+  
+  if (passwordField.type === "password") {
+      passwordField.type = "text";
+      visibleIcon.style.setProperty('display', 'none', 'important');
+      visibleIconOff.style.setProperty('display', 'initial', 'important');
+
+  } else {
+      passwordField.type = "password";
+      visibleIconOff.style.setProperty('display', 'none', 'important');
+      visibleIcon.style.setProperty('display', 'initial', 'important');
+  }
+}
 
 const useStyles = makeStyles((theme) => ({
  
@@ -30,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    backgroundColor: '#001E6F'
+    backgroundColor: '#00103B'
   },
 }));
 
@@ -48,29 +67,39 @@ export default function SignIn() {
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start"><AccountCircleTwoToneIcon /></InputAdornment>
+                ),
+            }}
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             id="email"
             label="Email Address"
             name="email"
             autoComplete="email"
             autoFocus
-            className = "text-field-style"
           />
           <TextField
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start"><LockTwoToneIcon /></InputAdornment>
+                ),
+            }}
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
             type="password"
-            id="password"
+            id="PasswordField"
             autoComplete="current-password"
-            className = "text-field-style"
           />
+          <div className="div-password-icon">
+            <VisibilityTwoToneIcon id="visible-icon" className="visible-password-position" onClick={view_password} />
+            <VisibilityOffTwoToneIcon id="visible-icon-off" className="visible-password-off-position" onClick={view_password} />
+          </div>
           <Button
             type="submit"
             fullWidth
